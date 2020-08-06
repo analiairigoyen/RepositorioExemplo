ccc762c44d24a50d4c50ab46f15dceb23d72f660
Analia Irigoyen <50586878+analiairigoyen@users.noreply.github.com>
2020-08-06 16:17:39 -0300

    Criação do testes unitário AB#3

---
+++ teste.js
@@ -0,0 +1,13 @@
+﻿const chai = require('chai');
+const expect = chai.expect;
+describe('Somas', () => ) {
+    it('soma de dois numeros - 2 e3', (done) => {
+        const resultado = somarNumeros(2, 3);
+        expect(resultado) = somarNumeros(2, 3);
+        expect(resultado).be.equal(5);
+        done();
+      )
+
+    }
+
+};
